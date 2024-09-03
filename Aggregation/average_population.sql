11b5ee540b151f191bc6420201e6a364a0d029a6
Shrikant Samarth <33530356+Shrikantsamarth@users.noreply.github.com>
2024-09-03 17:18:14 +0100

    Create average_population.sql

---
+++ Aggregation/average_population.sql
@@ -0,0 +1,24 @@
+/*
+Average Population
+==================
+Given a City table, whose fields are described as
+
++-------------+----------+
+| Field       | Type     |
++-------------+----------+
+| ID          | int(11)  |
+| Name        | char(35) |
+| CountryCode | char(3)  |
+| District    | char(20) |
+| Population  | int(11)  |
++-------------+----------+
+
+you have to print the average population of all cities, rounded down to the nearest integer.
+*/
+
+
+Solution-->>
+
+Select AVG(population)
+from City
+where district ='California'
